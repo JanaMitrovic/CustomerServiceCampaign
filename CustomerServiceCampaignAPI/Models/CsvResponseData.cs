ce132faeabde45c6b10aed5be10577a7d995ad4e
Jana Mitrovic <mitrovicjana0@gmail.com>
2023-10-16 12:22:10 +0200

    PurchaseAPIController cleaned and CsvResponseDate changed

--- CustomerServiceCampaignAPI/Models/CsvResponseData.cs
+++ CustomerServiceCampaignAPI/Models/CsvResponseData.cs
@@ -1,10 +1,19 @@
 ﻿using CustomerServiceCampaignAPI.Models.CustomerAPIData;
+using System.ComponentModel.DataAnnotations;
 
 namespace CustomerServiceCampaignAPI.Models
 {
     public class CsvResponseData
     {
-        public Purchase Purchase;
-        public Person Customer;
+        public int Id { get; set; }
+        public string AgentName { get; set; }
+        public string AgentSurname { get; set; }
+        public string AgentEmail { get; set; }
+        public string CampaignName { get; set; }
+        public int Price { get; set; }
+        public int Discount { get; set; }
+        public int PriceWithDiscount { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public Person Customer { get; set; }
     }
 }
